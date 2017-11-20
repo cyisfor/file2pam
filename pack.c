2dca851bd334e601d3624f092f848c027033c943
autocommit <autocommit>
2017-11-20 04:48:21 +0000

    auto 0 22 232

--- pack.c
+++ pack.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE // splice
+
 #include <arpa/inet.h> // htonl
 
 #include <sys/stat.h> // 
@@ -8,6 +10,9 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <assert.h>
+#include <unistd.h> // lseek
+#include <fcntl.h> // splice
+#include <stdlib.h> // mkstemp
 
 #define PUTLIT(lit) fwrite(lit,sizeof(lit)-1,1,stdout)
 
@@ -16,26 +21,32 @@ int main(int argc, char *argv[])
 	PUTLIT("P7\n");
 
 	struct stat info;
+	void* mem;
 	int res = fstat(0,&info);
 	if(res == 0 && info.st_size > 0) {
+		puts("boop");
 		mem = mmap(NULL,info.st_size,PROT_READ,MAP_PRIVATE,0,0);
 		assert(mem != MAP_FAILED);
 	} else {
 		char template[] = ".tmpXXXXXX";
 		int out = mkstemp(template);
 		unlink(template);
+		info.st_size = 0;
 		for(;;) {
 			ssize_t amt = splice(0, NULL, out, NULL, 0x100000, SPLICE_F_MOVE);
 			if(amt == 0) {
 				break;
 			}
+			fprintf(stderr,"reas %d\n",amt);
 			assert(amt > 0);
+			info.st_size += amt;
 		}
 		lseek(out, 0, SEEK_SET);
 		mem = mmap(NULL,info.st_size,PROT_READ,MAP_PRIVATE,out,0);
 		assert(mem != MAP_FAILED);
 	}
 
+	fprintf(stderr,"funt length %d\n",info.st_size);
 
 	// RGB_ALPHA = 4 bytes per pixel so /4 = /2 /2
 	// +4 for the sizeof the nsize
